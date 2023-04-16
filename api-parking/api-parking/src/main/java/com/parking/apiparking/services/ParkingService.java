8600cdb9fe6ea4f38bbfa2c8a70737209ab14ecf
Sandra Rodriguez <srodriguezg8@upao.edu.pe>
2023-04-16 00:59:28 -0500

    feat: Creado la clase parkingService. Implemento getAllCars y addCars

--- api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
@@ -1,4 +1,25 @@
 package com.parking.apiparking.services;
 
+import com.parking.apiparking.entities.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingService {
+    private List<Car>  parkingLot;
+    public ParkingService(){
+        this.parkingLot=new ArrayList<>();
+    }
+
+    //TODO:Como usuario, quiero poder ver la lista de todos los autos estacionados en el
+    //parqueo, para poder tener una visión general del estado actual del estacionamiento.
+    public List<Car> getAllCars(){
+        return this.parkingLot;
+    }
+
+    //TODO: Como usuario, quiero poder buscar un auto por su placa, para poder encontrar
+    //rápidamente un vehículo en particular
+    public void addCar(Car car){
+        this.parkingLot.add(car);
+    }
 }
