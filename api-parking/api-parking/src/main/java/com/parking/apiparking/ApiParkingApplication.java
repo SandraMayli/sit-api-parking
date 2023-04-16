49e2c505e6fd0cae74acb88a58b1231e7abfcae5
Sandra Rodriguez <srodriguezg8@upao.edu.pe>
2023-04-16 00:18:15 -0500

    Estructura del proyecto api parking - inicial

---
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/ApiParkingApplication.java
@@ -0,0 +1,12 @@
+package com.parking.apiparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParkingApplication.class, args);
+	}
+}
